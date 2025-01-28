6f1f043c8fc8533ec89f5187c0562eacb10134c0
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-29 01:24:45 +0530

    Backend basic server configuration

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Backend API working' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Backend is working on ${PORT}`);
+});
