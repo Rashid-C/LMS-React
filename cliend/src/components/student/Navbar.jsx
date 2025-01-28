7f6c032c4ef0ed721b094cd4464e28faa8d8f21b
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-29 03:12:41 +0530

    added clerk auth fuctionality

--- cliend/src/components/student/Navbar.jsx
+++ cliend/src/components/student/Navbar.jsx
@@ -1,8 +1,12 @@
 import {assets} from '../../assets/assets'
 import {Link, useLocation} from 'react-router-dom'
+import {  UserButton,useClerk,useUser } from "@clerk/clerk-react";
 const Navbar = () => {
   const location = useLocation();
   const isCourseListPage = location.pathname.includes('/course-list');
+
+  const {openSignIn} =useClerk()
+  const {user} =useUser()
  
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white':'bg-cyan-100/70'}`}>
@@ -12,8 +16,8 @@ const Navbar = () => {
           <button>Become Educator</button>
           | <Link to='/my-enrollments' >My Envirollments</Link>
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
-
+       { user? <UserButton/>: <button onClick={()=>openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+}
       </div>
       {/* for phone screen */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-green-500'>
