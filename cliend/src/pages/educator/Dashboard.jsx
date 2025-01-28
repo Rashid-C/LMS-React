437a229aee0d82af88675f78ea25edb5d514f033
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-29 00:46:22 +0530

    Created pages for admin & clients

---
+++ cliend/src/pages/educator/Dashboard.jsx
@@ -0,0 +1,8 @@
+
+const Dashboard = () => {
+  return (
+    <div> EDUCATOR Dashboard</div>
+  )
+}
+
+export default Dashboard
---
+++ cliend/src/pages/educator/MyCourses.jsx
@@ -0,0 +1,9 @@
+
+
+const MyCourses = () => {
+  return (
+    <div> EDUCATOR MyCourses</div>
+  )
+}
+
+export default MyCourses
