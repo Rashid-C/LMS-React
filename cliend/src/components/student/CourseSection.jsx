6227ea4ebe50053e5bc0b6290e654edbd510d471
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-30 00:26:52 +0530

    Add Course to CourseCart from  CourseSection and its static way

--- cliend/src/components/student/CourseSection.jsx
+++ cliend/src/components/student/CourseSection.jsx
@@ -1,15 +1,26 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import CourseCard from "./CourseCard";
 
 const CourseSection = () => {
+  const { allCourses } = useContext(AppContext);
   return (
     <div className="py-16 md:px-40 px-8">
-      <h2 className="text-3xl font-medium text-gray-800">Taught by Top Experts from</h2>
+      <h2 className="text-3xl font-medium text-gray-800">
+        Taught by Top Experts from
+      </h2>
       <p className="text-sm md:text-base text-gray-500 mt-3">
         Explore our highly-rated courses spanning diverse fields. From coding
-        and design to business and personal development, each course is
+        and design to <br /> business and personal development, each course is
         meticulously designed to deliver measurable results and elevate your
         skills.
       </p>
+      <div className="grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4">
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
 
       <Link
         to={"/course-list"}
