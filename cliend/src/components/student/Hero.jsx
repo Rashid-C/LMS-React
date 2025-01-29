d20f0eadee5dac8d15ef8fdf513767eb8c99013a
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-29 22:55:40 +0530

    Created Hero part & custome style

--- cliend/src/components/student/Hero.jsx
+++ cliend/src/components/student/Hero.jsx
@@ -1,9 +1,31 @@
-
+import { assets } from "../../assets/assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
+      <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto">
+        Shape your destiny with courses tailored to your aspirations—your
+        journey to <span className="text-blue-600">success starts here.</span>{" "}
+        <img
+          src={assets.sketch}
+          alt="sketch"
+          className="md:block hidden absolute -bottom-7 right-0"
+        />{" "}
+      </h1>
 
-export default Hero
+      <p className="md:block hidden text-gray-500 max-w-2xl mx-auto">
+        Join a dynamic learning experience where expert instructors, interactive
+        resources, and a supportive community come together to guide you toward
+        achieving your personal and professional goals with confidence and
+        clarity.
+      </p>
+
+      <p className="md:hidden text-gray-500 max-w-sm mx-auto">
+        Learn with experts, interactive tools, and a supportive community to
+        achieve your goals with confidence.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
